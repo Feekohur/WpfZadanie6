210644518634573fa63bab71576f498c8a5d51d2
Feekohur <dudadudadudadudada@gmail.com>
2023-01-19 22:40:09 +0100

    Dodaj pliki projektów.

---
+++ WpfZadanie6/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfZadanie6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            osoby.Add(new Osoba("Bogusław", "Łęcina", "blecina1@gmail.com", 100, "Białystok", 1));
+            osoby.Add(new Osoba("Dominik", "Jachaś", "dominogalakpizza@gmail.com", 0.00M, "Warszawa", 5));
+            osoby.Add(new Osoba("Adam", "Małysz", "amalysz@pb.edu.pl", 25.50M, "Kraków", 3));
+            osoby.Add(new Osoba("Robert", "Kubica", null, null, null, null));
+            regiony.Add("Białystok");
+            regiony.Add("Warszawa");
+            regiony.Add("Kraków");
+            regiony.Add("Poznań");
+            regiony.Add("Gdańsk");
+            regiony.Add("Brak");
+        }
+        Collection<Osoba> osoby { get; } = new ObservableCollection<Osoba>();
+        List<string> regiony { get; } = new List<string>();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            lista.ItemsSource = osoby;
+            region.ItemsSource = regiony;
+        }
+
+        private void Button_Usun(object sender, RoutedEventArgs e)
+        {
+            if(lista.SelectedItem != null)
+            {
+                osoby.RemoveAt(lista.SelectedIndex);
+            }
+
+        }
+
+        private void Button_Dodaj(object sender, RoutedEventArgs e)
+        {
+            osoby.Add(new Osoba());
+            lista.SelectedIndex = osoby.Count - 1;
+        }
+
+        private void Dane_szczegółówe_Click(object sender, RoutedEventArgs e)
+        {
+            if(Dane_szczegółówe.IsChecked == true)
+            {
+                Dodatkowe_dane.IsEnabled = true;
+            }
+            else
+            {
+                Dodatkowe_dane.IsEnabled = false;
+            }
+        }
+    }
+}
---
+++ WpfZadanie6/Osoba.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfZadanie6
+{
+    internal class Osoba : INotifyPropertyChanged
+    {
+        private string imie;
+        public string Imie
+        {
+            get { return imie; }
+            set { imie = value; ZmianaInterfejsu("Opis"); }
+        }
+        private string nazwisko;
+        public string Nazwisko
+        {
+            get { return nazwisko; }
+            set { nazwisko = value; ZmianaInterfejsu("Opis"); }
+        }
+        private string? email;
+        public string? Email
+        {
+            get { return email; }
+            set { email = value; ZmianaInterfejsu("Opis"); }
+        }
+        public decimal? kwota_wplaty { get; set; }
+        public string? region { get; set; }
+        public int? poziom_dostepu { get; set; }
+
+        public Osoba()
+        {
+
+        }
+        public Osoba(string i, string n, string? e, decimal? k_w, string? r, int? p_d)
+        {
+            imie = i;
+            nazwisko = n;
+            email = e;
+            kwota_wplaty = k_w;
+            region = r;
+            poziom_dostepu = p_d;
+        }
+
+        public string Opis { 
+            get
+            {
+                return imie + " " + nazwisko + " " + "(" + email + ")";
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void ZmianaInterfejsu(string property)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        public override string ToString()
+        {
+            return imie + " " + nazwisko + " " + "(" + email + ")";
+        }
+    }
+}
